6657f5025595c5b13fe27d957bfcc3875b3d1fd2
Isha-Chakravorty <ishachakravorty857@gmail.com>
2026-03-20 09:15:13 +0530

    UC2: Add Passenger Bogies to Train

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -6,14 +6,27 @@ public class TrainConsistManagementApp {
     public static void main(String[] args) {
 
         System.out.println("=======================================");
-        System.out.println(" UC1 - Initialize Train and Display Consist Summary ");
+        System.out.println(" UC2 - Add Passenger Bogies to Train ");
         System.out.println("=======================================\n");
 
-        // Create empty train consist
+        // Create train consist
         List<String> trainConsist = new ArrayList<>();
 
-        // Display initial state
-        System.out.println("Train initialized successfully.");
-        System.out.println("Initial Bogie Count: " + trainConsist.size());
+        // Add passenger bogies
+        trainConsist.add("B1");
+        trainConsist.add("B2");
+        trainConsist.add("B3");
+        trainConsist.add("B4");
+
+        // Display bogies
+        System.out.println("Passenger Bogies Added Successfully!\n");
+
+        System.out.println("Current Train Consist:");
+        for (String bogie : trainConsist) {
+            System.out.println(bogie);
+        }
+
+        // Display total count
+        System.out.println("\nTotal Bogies: " + trainConsist.size());
     }
 }
